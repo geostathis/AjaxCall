f2e2af27c3702f1e7788bc92c3783bd00905cb38
STATHIS GEORGIOS <gstathis@thess.geoanalysis>
2020-11-27 14:53:14 +0200

    promises

--- main.js
+++ main.js
@@ -1,20 +1,40 @@
 
-function getData(data){
-    $.ajax({
-        type:'GET',
-        url: 'https://api.github.com/repos/angular/angular.js/issues?per_page=10',
-        dataType:'json',
-        data:data,
-        async:false,
-        success:function(data){
-            data=JSON.stringify(data);
-            mydata=data;
-            console.log(mydata);
-        },
-        error: function (err){
-            alert(err);
-        }
-    });
-    
 
-}
+/*Εδω ορίζω το πρώτο promise που θα κάνει το request, εαν γίνει με επιτυχία , θα εκτελέσει το resolve 
+δηλαδή θα εκτελεστεί ότι είναι μέσα σto then . */
+var promise =new Promise(function (resolve,reject){
+    const getData_=fetch("https://api.github.com/repos/angular/angular.js/issues?per_page=10");
+    data=getData_.then(response=>{
+    window.data=response.json();
+    return data;
+});
+    if (data){
+        resolve(data);
+    }else{
+        reject(Error("Error"));
+    }
+});
+promise.then(function(result){
+    console.log(result);
+}).catch(function (err){
+    console.log(err);
+})
+
+var last={};
+//Το promise2 το οποίο στην ουσία εκτελεί το promise 1
+var promise2=new Promise(function(resolve,reject){
+    var p=promise;
+    if (p){
+        resolve(p);
+    }else{
+        reject(Error("Error"));
+    }
+})
+promise2.then(function(p){
+    var last=p[9]
+    document.getElementById("myObject").innerHTML="To html_url είναι: "+last['html_url'];
+  //  window.location.href = "www.google.com";
+}).catch(function (err){
+    console.log(err);
+})
+
