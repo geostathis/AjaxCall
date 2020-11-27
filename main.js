ee84d3cc9b768f02b26f41a0606e6c2ca5f07bc6
STATHIS GEORGIOS <gstathis@thess.geoanalysis>
2020-11-27 16:42:20 +0200

    fix bugs

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
-
-
+var last={};
 /*Εδω ορίζω το πρώτο promise που θα κάνει το request, εαν γίνει με επιτυχία , θα εκτελέσει το resolve 
 δηλαδή θα εκτελεστεί ότι είναι μέσα σto then . */
 var promise =new Promise(function (resolve,reject){
@@ -7,34 +6,48 @@ var promise =new Promise(function (resolve,reject){
     data=getData_.then(response=>{
     window.data=response.json();
     return data;
-});
-    if (data){
+    });
+    if (data)
         resolve(data);
-    }else{
+    else
         reject(Error("Error"));
-    }
-});
+    });
 promise.then(function(result){
-    console.log(result);
+    return result;
 }).catch(function (err){
     console.log(err);
 })
 
-var last={};
 //Το promise2 το οποίο στην ουσία εκτελεί το promise 1
 var promise2=new Promise(function(resolve,reject){
     var p=promise;
-    if (p){
+    if (p)
         resolve(p);
-    }else{
+    else{
         reject(Error("Error"));
     }
 })
 promise2.then(function(p){
-    var last=p[9]
+    var last=Object.assign({},p[p.length-1]);
+    console.log(last);
     document.getElementById("myObject").innerHTML="To html_url είναι: "+last['html_url'];
-  //  window.location.href = "www.google.com";
+    window.location.href=last['html_url'];
+    return last;
+    
 }).catch(function (err){
     console.log(err);
 })
+
+// var promise3=new Promise(function(resolve,reject){
+//     var p=promise3;
+//     if (p)
+//         resolve(p);
+//     else
+//         reject(Error("Error"));
+//   //  console.log(p);
+// });
+// promise3.then(function (last){console.log(last);}).catch(function (err){
+//     console.log(err);
+// })
+
 
